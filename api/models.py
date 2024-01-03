f7eb2385bd17c711a24d2c465da3dd01ab972d95
JohnMP <j_ponce3@u.pacific.edu>
2024-01-03 04:15:29 -0800

    Restarted database

--- api/models.py
+++ api/models.py
@@ -1,20 +1,17 @@
-from django.utils.crypto import get_random_string
 from django.db import models
-import string
 import random
-
+import string
 
 def generate_unique_code():
-    length = 8
+    length = 10
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if not Room.objects.filter(code=code).exists():
-            return code
-    
+        if Room.objects.filter(code=code).exists():
+            break
+    return code
+
 class Room(models.Model):
-    # code = models.CharField(
-    #     max_length=8, default=generate_unique_code, unique=True)
-    code = get_random_string(8, allowed_chars=string.ascii_uppercase + string.digits)
+    code = models.CharField(max_length=10, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
