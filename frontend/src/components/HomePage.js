53ed5730c242903779434ea6c7807e62a3a4d210
JohnMP <j_ponce3@u.pacific.edu>
2024-01-03 03:19:25 -0800

    Fixed routing problem

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default class HomePage extends Component {
+export default 
+class HomePage extends Component {
   constructor(props) {
     super(props);
   }
@@ -16,12 +12,10 @@ export default class HomePage extends Component {
   render() {
     return (
       <Router>
-        <Routes>
-          <Route path="/">
-            <p>This is the home page</p>
-          </Route>
-          <Route path="/join" element={RoomJoinPage} />
-          <Route path="/create" element={CreateRoomPage} />
+        <Routes>  {/* Similar to switch statements */}
+          <Route path="/" element={<p>This is the home page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
         </Routes>
       </Router>
     );
