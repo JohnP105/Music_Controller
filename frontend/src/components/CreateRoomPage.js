033cdcc2a3ede44fe1c9c2cb194277221f0617a6
JohnMP <j_ponce3@u.pacific.edu>
2024-01-04 21:33:10 -0800

    Change class components to functional ~ also added redirection when new room is created

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
@@ -10,113 +11,120 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
-export default class CreateRoomPage extends Component {
-  defaultVotes = 2;
+const CreateRoomPage = () => {
+  const navigate = useNavigate();
+  const defaultVotes = 2;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      guestCanPause: true, 
-      votesToSkip: this.defaultVotes,
-    };
+  const [roomState, setRoomState] = useState({
+    guestCanPause: true,
+    votesToSkip: defaultVotes,
+  });
 
-    this.handleVotesChange = this.handleVotesChange.bind(this);
-    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
-    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
-
-  }
-
-  handleVotesChange(e) {
-    this.setState({
+  const handleVotesChange = (e) => {
+    setRoomState({
+      ...roomState,
       votesToSkip: e.target.value,
     });
-  }
+  };
 
-  handleGuestCanPauseChange(e) {
-    this.setState({
-      guestCanPause: e.target.value === "true" ? true : false,
-    }); 
-  }
+  const handleGuestCanPauseChange = (e) => {
+    setRoomState({
+      ...roomState,
+      guestCanPause: e.target.value === "true",
+    });
+  };
 
-  handleRoomButtonPressed() {
+  const handleRoomButtonPressed = () => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        votes_to_skip: this.state.votesToSkip,
-        guest_can_pause: this.state.guestCanPause,
+        votes_to_skip: roomState.votesToSkip,
+        guest_can_pause: roomState.guestCanPause,
       }),
     };
     fetch("/api/create-room", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+      .then((data) => {
+        console.log(data);
+        navigate(`/room/${data.code}`); // Redirect to the created room
+      });
+  };
 
-  render() {
-    return (
-      <Grid container spacing={1}>
-        <Grid item xs={12} align="center">
-          <Typography component="h4" variant='h4'>
-            Create a Room
-          </Typography>
-        </Grid>
-
-        <Grid item xs={12} align="center">
-          <FormControl component="fieldsets">
-            <FormHelperText>
-              <div align="center">
-                Guest Control of Playback States
-              </div>
-            </FormHelperText>
-            <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
-              <FormControlLabel 
-                value="true" 
-                control={<Radio color="primary"/>}
-                label="Play/Pause"
-                labelPlacement="bottom"     
-              />
-              <FormControlLabel 
-                value="false" 
-                control={<Radio color="secondary"/>}
-                label="No Control"
-                labelPlacement="bottom"     
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-
-        <Grid item xs={12} align="center">
-          <FormControl>
-            <TextField 
-              required={true} 
-              type="number" 
-              defaultValue={this.defaultVotes}
-              inputProps={{
-                min: 1,
-                style: {textAlign: "center"},
-              }}
-              onChange={this.handleVotesChange}
-            />
-            <FormHelperText>
-              <div align="center">
-                Votes Required To Skip Song
-              </div>
-            </FormHelperText>
-          </FormControl>
-        </Grid>
-
-        <Grid item xs={12} align="center">
-          <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}> 
-            Create A Room
-          </Button>
-        </Grid>
-
-        <Grid item xs={12} align="center">
-          <Button color="secondary" variant="contained" to="/" component={Link}> 
-            Back
-          </Button>
-        </Grid>
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Create a Room
+        </Typography>
       </Grid>
-    );
-  }
-}
+
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldsets">
+          <FormHelperText>
+            <div align="center">Guest Control of Playback States</div>
+          </FormHelperText>
+          <RadioGroup
+            row
+            defaultValue="true"
+            onChange={handleGuestCanPauseChange}
+          >
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={12} align="center">
+        <FormControl>
+          <TextField
+            required={true}
+            type="number"
+            defaultValue={defaultVotes}
+            inputProps={{
+              min: 1,
+              style: { textAlign: "center" },
+            }}
+            onChange={handleVotesChange}
+          />
+          <FormHelperText>
+            <div align="center">Votes Required To Skip Song</div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={12} align="center">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleRoomButtonPressed}
+        >
+          Create A Room
+        </Button>
+      </Grid>
+
+      <Grid item xs={12} align="center">
+        <Button
+          color="secondary"
+          variant="contained"
+          component={Link}
+          to="/"
+        >
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CreateRoomPage;
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,22 +1,49 @@
-import React, {Component} from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default class Room extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            votesToSkip: 2,
-            guestCanPause: false,
-            isHost: false,
-        };
-    }
+const Room = (props) => {
+  const [roomDetails, setRoomDetails] = useState({
+    votesToSkip: 2,
+    guestCanPause: false,
+    isHost: false,
+  });
 
-    render() {
-        return (
-            <div>
-                <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause}</p>
-                <p>Host: {this.state.isHost}</p>
-            </div>
-        );
-    }
-}
+  const { roomCode } = props;
+
+  useEffect(() => {
+    const getRoomDetails = async () => {
+      try {
+        const response = await fetch(`/api/get-room?code=${roomCode}`);
+        const data = await response.json();
+        setRoomDetails({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+      } catch (error) {
+        console.error("Error fetching room details:", error);
+      }
+    };
+
+    getRoomDetails();
+  }, [roomCode]);
+
+  return (
+    <div>
+      <h3>{roomCode}</h3>
+      <p>Votes: {roomDetails.votesToSkip}</p>
+      <p>Guest Can Pause: {roomDetails.guestCanPause.toString()}</p>
+      <p>Host: {roomDetails.isHost.toString()}</p>
+    </div>
+  );
+};
+
+const RoomWrapper = () => {
+  const { roomCode } = useParams();
+
+  return <Room roomCode={roomCode} />;
+};
+
+export default RoomWrapper;
+
+
